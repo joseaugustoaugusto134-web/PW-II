8ab7682dcb8f16bcedfc7fd6531b12227d250a69
MinakoGW <joseaugusto.augusto134@gmail.com>
2026-03-19 00:09:02 -0300

    Add files via upload

--- PW II/exercicios/ex5/index.php
+++ PW II/exercicios/ex5/index.php
@@ -5,7 +5,8 @@ require __DIR__ . "/../../source/autoload.php";
 use source\Models\Hospital\Patient;
 use source\models\Hospital\Doctor;
 
-$patient = new Patient("20/10/2000", "sla tlgd");
-$doctor = new Doctor("121312", "cardiologista");
+$patient = new Patient("20/10/2000", "sla tlgd", "Josias", "Josias@gmail.com", 1, "josiasomelhor", "fotodeperfildojosias.jpeg");
+$doctor = new Doctor("121312", "cardiologista", "Doutor Matias", "DoutorMatiasCuraTudo@gmail.com", 1, "MatiasODoutor", "FotoDeMatias.png");
 
-
+$patient->show();
+$doctor->show();
