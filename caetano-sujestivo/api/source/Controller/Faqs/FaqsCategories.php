18cc708b7b97a341166a80c63394ee98ff5b926d
MinakoGW <joseaugusto.augusto134@gmail.com>
2026-05-28 04:26:53 -0300

    Add files via upload

--- caetano-sujestivo/api/source/Controller/Faqs/FaqsCategories.php
+++ caetano-sujestivo/api/source/Controller/Faqs/FaqsCategories.php
@@ -7,30 +7,37 @@ use Source\Models\Faq\FaqCategory;
 
 class FaqsCategories extends Api
 {
-    public function faqsCategoriesList(array $data)
+    //listAll sem Model.php
+    //public function faqsCategoriesList(array $data)
+    //{
+    //    $faqCategory = new FaqCategory();
+    //    $response = $faqCategory->listAll();
+
+    //    $this->call("200", "success", "Lista de categoria de FAQs", "success"
+    //    )->back($response);
+    //}
+
+    public function listAll (array $data): void
     {
         $faqCategory = new FaqCategory();
-        $response = $faqCategory->listAll();
-
-        $this->call("200", "success", "Lista de categoria de FAQs", "success"
-        )->back($response);
+        $this->call(200,"success","Lista de categorias de FAQ","success")->back($faqCategory->selectAll());
     }
 
-    public function faqsCategoriesListById(array $data)
-    {
-        if(!filter_var($data["categoryId"], FILTER_VALIDATE_INT)) 
-        {
-            $this->call(
-                400,
-                "bad_request",
-                "ID da categoria é obrigatório e deve ser um número inteiro",
-                "error"
-            )
-                ->back(null);
-            return;
-        }
+    //public function faqsCategoriesListById(array $data)
+    //{
+    //    if(!filter_var($data["categoryId"], FILTER_VALIDATE_INT)) 
+    //    {
+    //        $this->call(
+     //           400,
+     //           "bad_request",
+     //           "ID da categoria é obrigatório e deve ser um número inteiro",
+     //           "error"
+     //       )
+     //           ->back(null);
+      //      return;
+      //  }
 
-        $faqCategory = new FaqCategory();
+       /* $faqCategory = new FaqCategory();
 
         $faqCategory = $faqCategory->listById($data["categoryId"]);
         if(!$faqCategory) {
@@ -68,4 +75,78 @@ class FaqsCategories extends Api
            ->back($faqCategory);
     }
     
+}*/
+
+    public function listById(array $data): void
+    {
+
+        if(!isset($data["categoryId"]) || empty($data["categoryId"]) || !filter_var($data["categoryId"], FILTER_VALIDATE_INT)) {
+            $this->call(
+                400,
+                "bad_request",
+                "ID da categoria é obrigatório e deve ser um número inteiro",
+                "error"
+            )->back(null);
+            return;
+        }
+
+        $faqCategory = new FaqCategory();
+        if(!$faqCategory->selectById($data["categoryId"])) {
+            $this->call(
+                404,
+                "not_found",
+                "Categoria não encontrada",
+                "error"
+            )->back(null);
+            return;
+        }
+
+        $response = [
+            "id" => $faqCategory->getId(),
+            "name" => $faqCategory->getName(),
+            "active" => $faqCategory->getActive()
+        ];
+
+        $this->call(200,"success","Categoria encontrada","success")->back($response);
+    }
+
+    public function insert (array $data): void
+    {
+        if(!$this->validate($data)){
+            $this->call(
+                400,
+                "bad_request",
+                "O campo name é obrigatório",
+                "error"
+            )->back();
+            return;
+        }
+
+        $faqCategory = new FaqCategory(
+            null,
+            $data["name"]
+        );
+
+        if(!$faqCategory->insert()){
+            $this->call(500, "internal_server_error", $faqCategory->getErrorMessage(), "error")->back();
+            return;
+        }
+        $response = [
+            "id" => $faqCategory->getId(),
+            "name" => $faqCategory->getName()
+        ];
+
+        $this->call(201,"success","FAQ inserido com sucesso","success")->back($response);
+
+    }
+
+    public function validate (array $data): bool
+    {
+        if(!isset($data["name"])  ||
+            empty($data["name"])) 
+        {
+            return false;
+        }
+        return true;
+    }
 }
